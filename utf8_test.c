d76b65ca208b2cde1c34505a3341bb4add27d4eb
Joel Lehtonen <joel.lehtonen@iki.fi>
2009-07-31 13:48:06 +0300

    Re-indented using Linux style.

--- utf8_test.c
+++ utf8_test.c
@@ -6,23 +6,23 @@ const int true = 1;
 
 int main(int argc, char **argv)
 {
-  if (argc != 2) errx(1, "Usage: %s UTF-8_TEXT_FILE", argv[0]);
+	if (argc != 2) errx(1, "Usage: %s UTF-8_TEXT_FILE", argv[0]);
 
-  FILE *file = fopen(argv[1], "rb");
-  if ( file == NULL) err(1,"Tiedostoa ei saanut avattua");
+	FILE *file = fopen(argv[1], "rb");
+	if ( file == NULL) err(1,"Tiedostoa ei saanut avattua");
   
-  unsigned char buf[UTF8_MAX_BYTES];
+	unsigned char buf[UTF8_MAX_BYTES];
 
-  while (true) {
-    int n = utf8_fgetc(file, buf);
+	while (true) {
+		int n = utf8_fgetc(file, buf);
     
-    if (n == UTF8_ERR_NO_DATA && feof(file) ) break; // normal EOF
-    if (n < 0) errx(2,"Vika tiedostossa.");
+		if (n == UTF8_ERR_NO_DATA && feof(file) ) break; // normal EOF
+		if (n < 0) errx(2,"Vika tiedostossa.");
 
-    fwrite(buf, 1, n, stdout);
-    putchar(' ');
-  }
+		fwrite(buf, 1, n, stdout);
+		putchar(' ');
+	}
 
-  return 0;
+	return 0;
 
 }
